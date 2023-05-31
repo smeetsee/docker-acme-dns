93f76b351b2325b6c56a6cc571ea932b5b8cd7e4
Xander Smeets <xander@smeets.re>
2023-05-31 22:28:33 +0200

    Remove directory creation from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-mkdir -p /home/container/etc && mkdir -p /home/container/var/lib && ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
